a1d130a206ec1e9adeaf76cb5c41b90f7f46c9cd
DaniahJ <109613719+DaniahJ@users.noreply.github.com>
2025-03-18 22:55:54 -0400

    passing test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-//  hello 
 import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
@@ -9,23 +8,26 @@ import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
+//import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { Button } from "react-bootstrap";
+import Cupcake from "./cupcakeIMG.jpg";
 
 function App(): React.JSX.Element {
+    3;
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
             <ColoredBox></ColoredBox>
             <hr></hr>
-            <ShoveBox></ShoveBox>
+            {/* <ShoveBox></ShoveBox> */}
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -38,6 +40,27 @@ function App(): React.JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
+            <hr />
+            <Button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <hr />
+            <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+            </ul>
+            <img
+                style={{ width: "20%" }}
+                src={Cupcake}
+                alt="A picture of a cupcake"
+            />
         </div>
     );
 }
